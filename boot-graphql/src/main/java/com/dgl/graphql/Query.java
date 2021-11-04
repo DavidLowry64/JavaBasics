19857db1d215bc2c7f91cd988ec0cdc12d27911a
David Lowry <DavidLowry64@gmail.com>
2021-11-03 22:54:44 -0400

    Initial Commit

---
+++ boot-graphql/src/main/java/com/dgl/graphql/Query.java
@@ -0,0 +1,16 @@
+package com.dgl.graphql;
+
+
+
+import org.springframework.stereotype.Component;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
